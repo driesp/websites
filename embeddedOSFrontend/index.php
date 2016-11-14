7543bead810c889a6e212189ecc7a5ab19b91f62
driesp - Dries Peeters <dries.peeters@dspautomation.be>
2016-11-14 17:58:29 +0100

    something

--- embeddedOSFrontend/index.php
+++ embeddedOSFrontend/index.php
@@ -43,14 +43,14 @@
             <h1 class="cover-heading">Alle Labo's</h1>
             <p class="lead">
               <div class="alert alert-info" role="alert">
-                <div class="progress">
-                  <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-                    60%
-                  </div>
-                </div>
+
                 <div class="input-group">
                   <span class="input-group-addon" id="basic-addon1">Temperature</span>
-                  <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
+                  <div class="progress">
+                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                      60%
+                    </div>
+                  </div>
                 </div>
 
               </div>
