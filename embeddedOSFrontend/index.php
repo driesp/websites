04395b046f2e3af7ce877de8e2b10b2f31842fa4
driesp - Dries Peeters <dries.peeters@dspautomation.be>
2016-11-15 22:04:53 +0100

    something

--- embeddedOSFrontend/index.php
+++ embeddedOSFrontend/index.php
@@ -72,7 +72,7 @@
                       <?php
                       $output=array();
                       $rv=0;
-                      exec("sudo -u online_tool_user -c '/home/pi/rpiShieldDriver/ADT7310/./program 2>&1' ",$output,$rv);
+                      exec("sudo -u root -c '/home/pi/rpiShieldDriver/ADT7310/./program 2>&1' ",$output,$rv);
                       //echo(%rv);
                       foreach($output as $line)
                       {
