a0643f7a27e19771c0c32d97bcc8ee40e40c819a
driesp - Dries Peeters <dries.peeters@dspautomation.be>
2016-11-15 21:44:00 +0100

    something

--- embeddedOSFrontend/index.php
+++ embeddedOSFrontend/index.php
@@ -70,8 +70,9 @@
                   <div class="progress">
                     <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
                       <?php
-                      $result = shell_exec("~/rpiShieldDriver/ADT7310/./program");
-                      echo($result);
+                      $output=array();
+                      $rv=0;
+                      exec('~/rpiShieldDriver/ADT7310/./readcard',$output,$rv);
                        ?>
                     </div>
                   </div>
