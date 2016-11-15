c7c056b9e26b10b9410d691cb439afcaf25b9910
driesp - Dries Peeters <dries.peeters@dspautomation.be>
2016-11-15 21:45:12 +0100

    something

--- embeddedOSFrontend/index.php
+++ embeddedOSFrontend/index.php
@@ -72,7 +72,7 @@
                       <?php
                       $output=array();
                       $rv=0;
-                      exec('~/rpiShieldDriver/ADT7310/./readcard',$output,$rv);
+                      exec('~/rpiShieldDriver/ADT7310/./program',$output,$rv);
                       foreach($output as $line)
                       {
                         echo("$line\t");
