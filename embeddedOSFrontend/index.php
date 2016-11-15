dbb2fb6cb01b62fec4f70c4c96f3bf922553edef
driesp - Dries Peeters <dries.peeters@dspautomation.be>
2016-11-15 22:08:28 +0100

    something

--- embeddedOSFrontend/index.php
+++ embeddedOSFrontend/index.php
@@ -72,7 +72,7 @@
                       <?php
                       $output=array();
                       $rv=0;
-                      exec("sudo -u root /home/pi/rpiShieldDriver/ADT7310/./program 2>&1 ",$output,$rv);
+                      exec("sudo -u root -S /home/pi/rpiShieldDriver/ADT7310/./program 2>&1 < /home/pi/.sudopass/sudopass.secret",$output,$rv);
                       //echo(%rv);
                       foreach($output as $line)
                       {
